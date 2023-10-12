64e27950821e32b84738eb073bd5f3331b5c1565
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-10-12 12:50:26 +0200

    server update

--- server.js
+++ server.js
@@ -9,22 +9,26 @@ const router = require("./router/router.js");
 const PORT = 3000;
 const HOST = '127.0.0.1'
 
-app.use(express.static('public')); 
+app.use(express.static('public'))
+app.use(useragent.express());; 
 app.use('/site', router);
 app.use(express.urlencoded({extended : true}));
 
+
 app.get("/", (req, res) => {
 
+    const useragent = req.useragent;
 
     if(useragent.isMobile){
-        res.sendFile(path.join(__dirname, 'public', 'm.index.html'))    
-    } else {
+        res.sendFile(path.join(__dirname, 'public', 'm.index.html'))
+    } else{
         res.sendFile(path.join(__dirname, 'public', 'index.html'))
     }
 
-    
-
+            
+        
 })
+
 
 app.listen(PORT, HOST,() => {
     console.log(`Server is now running on ${PORT}`)
