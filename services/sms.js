4c48d6f017d773a75dda9461186cd7c2045d2eef
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-11-01 21:02:56 +0100

    hello

--- services/sms.js
+++ services/sms.js
@@ -1,11 +1,16 @@
 
+
+// Twilio oplysninger fra min (ottos bruger)
 const accountSid = 'ACc51e52fb55e9f546269424e7756bdaf9';
 const authToken = 'abf8956e0b040a230e8621413741aa4d';
 
+// opsætter twilio client
 const client = require('twilio')(accountSid, authToken);
 
+//opstiller funktion som tager to variable og sender sender en sms til den givne modtager med den givne tekst 
 function SendText(text, receiver){
 
+  // sender sms
     client.messages
     .create({
        body: text,
@@ -16,4 +21,5 @@ function SendText(text, receiver){
     
 }
 
+// eksporterer funktionen så den kan bruges i andre filer
 module.exports = { SendText };
