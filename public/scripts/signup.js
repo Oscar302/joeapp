ad4b2854325794869ab291027d08dcaf069ad561
Oscar302 <osma22ad@student.cbs.dk>
2023-11-03 19:49:35 +0100

    små tilføjelser

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,15 +1,18 @@
+// funktionen tager teksten fra username, password og email og gemmer det i localstorage (kan sagtens tilføje dem alle)
+// grundet "require" i HTML, kan man ikke oprette sig uden at opfylde de felter hvortil require er brugt
+function createUser() {
+    const username = document.getElementById('username-sign-up').value;
+    const password = document.getElementById('password-sign-up').value;
+    const email = document.getElementById('email-sign-up').value;
 
-// make a sign up form that creates a user with the properties from user in localstorage
-// and then redirects to the login page
+    
+    const user = {
+        username: username,
+        password: password,
+        email: email
+    };
 
+    const userJSON = JSON.stringify(user);
+    localStorage.setItem('user', userJSON);
 
-const user = {firstname: "John", lastname : "Doe", username: "JohnDoe",  email : "joedoe@gmail.com", password : "kodeord", age : 21, phone : 11112222, address : "Solbjerg Plads 12", city : "Frederiksberg", zip : 2000, country : "Denmark"};
-// make a sign up form
-
-/*
-function createuser() {
-    window.location.href = "login.html";
-}
-
-createuser();
-*/
+} 
