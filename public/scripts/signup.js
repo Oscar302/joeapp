271564861c2c0f1f8045c3691d6f94a3ecfe96ef
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-11-03 10:45:07 +0100

    hello

---
+++ public/scripts/signup.js
@@ -0,0 +1,5 @@
+
+const user = {firstname: "John", lastname : "Doe", email : "joedoe@gmail.com", password : "kodeord", age : 21, phone : 11112222, address : "Solbjerg Plads 12", city : "Frederiksberg", zip : 2000, country : "Denmark"};
+
+
+
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+// Description: This is the main server file for the application. It will be used to run the server and handle all the requests.
 const express = require("express");
 const app = express();
 const path = require("path");
 
+// Importerer router filen
 const router = require("./router/router.js");
 
+// Sætter port og host
 const PORT = 3000;
 const HOST = "127.0.0.1";
 
+
+// Importerer sqlite3
+const sqlite3 = require("sqlite3").verbose();
+
+// Opretter database
+const db = new sqlite3.Database("./db/db.sqlite");
+
+//Nedstående funktiok kan køre SQL statements
+db.serialize(function () {
+  // Tabellen indeholder primærnøgle id, url, tidspunkt og caption
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, email TEXT, password TEXT, age INTEGER, phone INTEGER, address TEXT, city TEXT, zip INTEGER, country TEXT)"
+  );
+});
+
+// Sætter public folderen som statisk (ikke ejs filer)
 app.use(express.static("public"));
 
 app.use("/site", router);
