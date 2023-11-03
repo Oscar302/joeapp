dbd0f4c7cfe6d7807c7ab5b758a81fdd6c34a602
Oscar302 <osma22ad@student.cbs.dk>
2023-11-03 11:20:03 +0100

    :besked:

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,5 +1,15 @@
 
-const user = {firstname: "John", lastname : "Doe", email : "joedoe@gmail.com", password : "kodeord", age : 21, phone : 11112222, address : "Solbjerg Plads 12", city : "Frederiksberg", zip : 2000, country : "Denmark"};
+// make a sign up form that creates a user with the properties from user in localstorage
+// and then redirects to the login page
 
 
+const user = {firstname: "John", lastname : "Doe", username: "JohnDoe",  email : "joedoe@gmail.com", password : "kodeord", age : 21, phone : 11112222, address : "Solbjerg Plads 12", city : "Frederiksberg", zip : 2000, country : "Denmark"};
+// make a sign up form
 
+/*
+function createuser() {
+    window.location.href = "login.html";
+}
+
+createuser();
+*/
