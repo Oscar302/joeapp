3b30814ad63aa81b916e3b80c806bacba4601b79
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-12-13 15:23:50 +0100

    hello

--- services/sms.js
+++ services/sms.js
@@ -1,7 +1,9 @@
+const ecosystemConfig = require('../ecosystem.config');
+
 require('dotenv').config();
 // sætter twilio oplysninger i variabler
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN_TWI;
+const accountSid = ecosystemConfig.apps[0].ACCOUNT_SID;
+const authToken = ecosystemConfig.apps[0].AUTH_TOKEN_TWI;
 
 //opstiller funktion som tager to variable og sender sender en sms til den givne modtager med den givne tekst 
 async function SendText(text, number, sender, receiver){
