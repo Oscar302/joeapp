aa3a6f8a7f0e19231629e554e1cedf914ad1d7db
Otto Hother Sørensen <ottohothersorensen@Ottos-MacBook-Pro.local>
2023-12-13 15:14:22 +0100

    api

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,6 @@ module.exports = {
         instances: "max",
         ACCOUNT_SID : "AC044fb01afdc5d1d9e686400062ca3d05",
         AUTH_TOKEN_TWI : "5eaabdb9d75632ceb45454c3d2a05e40",
-        API_KEY : "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",
+        API_KEY : process.env.API_KEY,
       }]
 }
